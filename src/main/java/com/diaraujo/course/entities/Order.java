e074dbe00647511b3a4552256b40273c6dea8106
Diogo de Araújo <diogodearaujo21@yahoo.com.br>
2020-06-18 15:09:03 -0300

    OrderStatus enum

--- src/main/java/com/diaraujo/course/entities/Order.java
+++ src/main/java/com/diaraujo/course/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.diaraujo.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -25,6 +26,7 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -33,11 +35,12 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User user) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User user) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = user;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -62,6 +65,16 @@ public class Order implements Serializable {
 
 	public void setUser(User user) {
 		this.client = user;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
