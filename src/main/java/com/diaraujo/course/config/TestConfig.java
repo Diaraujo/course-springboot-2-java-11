d7d442bfdf69dad6d0ceb558ff01b4c0f7ff7bad
Diogo de Araújo <diogodearaujo21@yahoo.com.br>
2020-06-22 17:49:36 -0300

    OrderItem, many-to-many association with extra attributes Part 2

--- src/main/java/com/diaraujo/course/config/TestConfig.java
+++ src/main/java/com/diaraujo/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.diaraujo.course.entities.Category;
 import com.diaraujo.course.entities.Order;
+import com.diaraujo.course.entities.OrderItem;
 import com.diaraujo.course.entities.Product;
 import com.diaraujo.course.entities.User;
 import com.diaraujo.course.entities.enums.OrderStatus;
 import com.diaraujo.course.repositories.CategoryRepository;
+import com.diaraujo.course.repositories.OrderItemRepository;
 import com.diaraujo.course.repositories.OrderRepository;
 import com.diaraujo.course.repositories.ProductRepository;
 import com.diaraujo.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRespository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,6 +74,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRespository.saveAll(Arrays.asList(oi1, oi2,oi3,oi4));
 	}
 	
 }
